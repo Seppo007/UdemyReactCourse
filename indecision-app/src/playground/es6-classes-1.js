4ede9e0e711f2acc6241885ed2c9f9f365656564
Sebastian Schalow <schalowseb@gmail.com>
2018-01-29 20:12:01 +0100

    added subclasses and use !! for shorthand evaluation of a string to boolean

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -1,3 +1,4 @@
+// ES6 Class
 class Person {
   constructor(name = 'Anonymous', age = 0) {
     this.name = name;
@@ -5,7 +6,7 @@ class Person {
   };
 
   getGreeting() {
-    return `Hi ${this.name}!`;
+    return `Hi. I am ${this.name}!`;
   }
 
   getDescription() {
@@ -13,6 +14,48 @@ class Person {
   }
 }
 
-const me = new Person('Sebastian Schalow', 29);
-console.log(me.getGreeting());
+// Subclass
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+
+  hasMajor() {
+    return !!this.major;
+  }
+
+  getDescription() {
+    let description = super.getDescription();
+    if (this.hasMajor()) {
+      description += ` He studied ${this.major}.`;
+    }
+    return description;
+  }
+}
+
+class Traveler extends Person {
+
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+
+  getGreeting() {
+    let greeting = super.getGreeting();
+    if (this.homeLocation) {
+      greeting += ` I'm visiting from ${this.homeLocation}.`;
+    }
+    return greeting;
+  }
+
+}
+
+const me = new Student('Sebastian Schalow', 29, 'Computer Science');
 console.log(me.getDescription());
+
+const other = new Traveler();
+console.log(other.getGreeting());
+
+const traveler = new Traveler('Sebastian Schalow', 29, 'Wolfsburg');
+console.log(traveler.getGreeting());
