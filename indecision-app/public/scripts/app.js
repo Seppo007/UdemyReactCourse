564a1e28e05c91a82efc6848496ec449876ff025
Sebastian Schalow <schalowseb@gmail.com>
2018-02-03 22:08:11 +0100

    solved 10th challenge. write handler Methods and rebind this pointer in constructor

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -11,13 +11,33 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 var Counter = function (_React$Component) {
   _inherits(Counter, _React$Component);
 
-  function Counter() {
+  function Counter(props) {
     _classCallCheck(this, Counter);
 
-    return _possibleConstructorReturn(this, (Counter.__proto__ || Object.getPrototypeOf(Counter)).apply(this, arguments));
+    var _this = _possibleConstructorReturn(this, (Counter.__proto__ || Object.getPrototypeOf(Counter)).call(this, props));
+
+    _this.handleAddOne = _this.handleAddOne.bind(_this);
+    _this.handleMinusOne = _this.handleMinusOne.bind(_this);
+    _this.handleReset = _this.handleReset.bind(_this);
+    return _this;
   }
 
   _createClass(Counter, [{
+    key: 'handleAddOne',
+    value: function handleAddOne() {
+      console.log('handleAddOne');
+    }
+  }, {
+    key: 'handleMinusOne',
+    value: function handleMinusOne() {
+      console.log('handleMinusOne');
+    }
+  }, {
+    key: 'handleReset',
+    value: function handleReset() {
+      console.log('handleReset');
+    }
+  }, {
     key: 'render',
     value: function render() {
       return React.createElement(
@@ -30,17 +50,17 @@ var Counter = function (_React$Component) {
         ),
         React.createElement(
           'button',
-          null,
+          { onClick: this.handleAddOne },
           '+1'
         ),
         React.createElement(
           'button',
-          null,
+          { onClick: this.handleMinusOne },
           '-1'
         ),
         React.createElement(
           'button',
-          null,
+          { onClick: this.handleReset },
           'reset'
         )
       );
@@ -49,5 +69,9 @@ var Counter = function (_React$Component) {
 
   return Counter;
 }(React.Component);
+
+// Create three methods: handleAddOne, handleMinusOne, handleReset
+// Use console.log to print method name
+// Wire up onClick & bind in the constructor function
 
 ReactDOM.render(React.createElement(Counter, null), document.getElementById('app'));
