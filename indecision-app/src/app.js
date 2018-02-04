79d33fc1c8c03dea7201599e67733351f2aa7d14
Sebastian Schalow <schalowseb@gmail.com>
2018-02-04 13:18:42 +0100

    add localStorage functionality. Clear input if there wasn't an error on submitting. Show a message if there are no options

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -11,11 +11,22 @@ class IndecisionApp extends React.Component {
   }
 
   componentDidMount() {
-    console.log('fetching data');
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+      if (options) {
+        this.setState(() => ({options}));
+      }
+    } catch (e) {
+      // Do nothing
+    }
   }
 
   componentDidUpdate(prevProps, prevState, prevContext) {
-    console.log('saving data');
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
   }
 
   componentWillUnmount() {
@@ -98,6 +109,8 @@ const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 &&
+      <p>Please add an option to get started!</p>}
       {props.options.map(
         (option) =>
           <Option
@@ -135,6 +148,11 @@ class AddOption extends React.Component {
     const error = this.props.handleAddOption(option);
 
     this.setState(() => ({error}));
+
+    if (!error) {
+      e.target.elements.option.value = '';
+    }
+
   }
 
   render() {
