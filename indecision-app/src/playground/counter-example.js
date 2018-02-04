6127888609dbac6a0b4ff94206adbcf9ccb95286
Sebastian Schalow <schalowseb@gmail.com>
2018-02-04 13:34:24 +0100

    solve challenge 14. Add localStorage functionality to Counter example

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -7,8 +7,24 @@ class Counter extends React.Component {
     this.handleMinusOne = this.handleMinusOne.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.state = {
-      count: props.count,
+      count: 0,
     };
+  }
+
+  componentDidMount() {
+    const stringCount = localStorage.getItem('count');
+    if (stringCount) {
+      const count = parseInt(stringCount);
+      if (!isNaN(count)) {
+        this.setState(() => ({count}));
+      }
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState, prevContext) {
+    if (prevState.count !== this.state.count) {
+      localStorage.setItem('count', this.state.count);
+    }
   }
 
   handleAddOne() {
@@ -38,10 +54,6 @@ class Counter extends React.Component {
     );
   }
 }
-
-Counter.defaultProps = {
-  count: 0,
-};
 
 // Create three methods: handleAddOne, handleMinusOne, handleReset
 // Use console.log to print method name
