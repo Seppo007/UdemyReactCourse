b441f293eb33455649e7d2b8ccf30c8f54598c4d
Sebastian Schalow <schalowseb@gmail.com>
2018-04-14 20:06:36 +0200

    add expensify-app

---
+++ expensify-app/src/components/IndecisionApp.js
@@ -0,0 +1,104 @@
+import React from 'react';
+
+import Action from './Action';
+import AddOption from './AddOption';
+import Header from './Header';
+import Options from './Options';
+import OptionModal from './OptionModal';
+
+class IndecisionApp extends React.Component {
+
+  state = {
+    options: [],
+    selectedOption: undefined,
+  };
+
+  handleClearSelectedOption = () => {
+    this.setState(() => ({selectedOption: undefined}));
+  };
+
+  handleDeleteOptions = () => {
+    this.setState(() => ({options: []}));
+  };
+
+  handleDeleteOption = (optionToRemove) => {
+    this.setState((prevState) => ({
+      options: prevState.options.filter(
+        (option) => (optionToRemove !== option)),
+    }));
+  };
+
+  handlePick = () => {
+    const optLength = this.state.options.length;
+    const randomNumber = (Math.floor(Math.random() *
+      optLength));
+    this.setState(() => ({
+      selectedOption: this.state.options[randomNumber],
+    }));
+  };
+
+  handleAddOption = (option) => {
+    if (!option) {
+      return 'Enter valid value to add item';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This is option already exists';
+    }
+
+    this.setState((prevState) => ({options: prevState.options.concat(option)}));
+  };
+
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+      if (options) {
+        this.setState(() => ({options}));
+      }
+    } catch (e) {
+      // Do nothing
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState, prevContext) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
+  }
+
+  componentWillUnmount() {
+    console.log('IndecisionApp will unmount');
+  }
+
+  render() {
+    return (
+      <div>
+        <Header subtitle="Put your life in the hands of a computer"/>
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}/>
+          <div className="widget">
+            <Options
+              options={this.state.options}
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption}
+            />
+            <AddOption
+              handleAddOption={this.handleAddOption}/>
+            <OptionModal
+              selectedOption={this.state.selectedOption}
+              handleClearSelectedOption={this.handleClearSelectedOption}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+IndecisionApp.defaultProps = {
+  options: [],
+};
+
+export default IndecisionApp;
