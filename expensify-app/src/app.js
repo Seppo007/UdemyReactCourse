51e18ae0b25ce8cc6a97b2672c991538a0224b35
Sebastian Schalow <schalowseb@gmail.com>
2018-04-14 21:00:35 +0200

    Add Switch to render a route exclusively if it matches

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 const ExpenseDashboardPage = () => (
   <div>
@@ -28,14 +28,21 @@ const HelpPage = () => (
   </div>
 );
 
+const NotFoundPage = () => (
+  <div>
+    404!
+  </div>
+);
+
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true}/>
       <Route path="/create" component={AddExpensePage}/>
       <Route path="/edit" component={EditExpensePage}/>
       <Route path="/help" component={HelpPage}/>
-    </div>
+      <Route component={NotFoundPage}/>
+    </Switch>
   </BrowserRouter>
 );
 
