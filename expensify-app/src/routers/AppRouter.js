ad2277a7fe5b3c658558bafa1506652d9c67b3c7
Sebastian Schalow <schalowseb@gmail.com>
2018-04-15 16:59:43 +0200

    Preparations for React-Router. Delete unneccesary files

--- expensify-app/src/routers/AppRouter.js
+++ expensify-app/src/routers/AppRouter.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {BrowserRouter, NavLink, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from "../components/AddExpensePage";
 import EditExpensePage from "../components/EditExpensePage";
 import HelpPage from "../components/HelpPage";
 import NotFoundPage from "../components/NotFoundPage";
+import Header from "../components/Header";
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -19,15 +20,6 @@ const AppRouter = () => (
       </Switch>
     </div>
   </BrowserRouter>
-);
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-  </header>
 );
 
 export default AppRouter;
