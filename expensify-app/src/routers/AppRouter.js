03e8c94fd8f1acc321371e4ed4f284d7fa131431
Sebastian Schalow <schalowseb@gmail.com>
2018-04-15 16:01:33 +0200

    Remove Edit from Header. Extends Edit Component with information from React-Router

--- expensify-app/src/routers/AppRouter.js
+++ expensify-app/src/routers/AppRouter.js
@@ -13,7 +13,7 @@ const AppRouter = () => (
       <Switch>
         <Route path="/" component={ExpenseDashboardPage} exact={true}/>
         <Route path="/create" component={AddExpensePage}/>
-        <Route path="/edit" component={EditExpensePage}/>
+        <Route path="/edit/:id" component={EditExpensePage}/>
         <Route path="/help" component={HelpPage}/>
         <Route component={NotFoundPage}/>
       </Switch>
@@ -26,7 +26,6 @@ const Header = () => (
     <h1>Expensify</h1>
     <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
     <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
     <NavLink to="/help" activeClassName="is-active">Help</NavLink>
   </header>
 );
