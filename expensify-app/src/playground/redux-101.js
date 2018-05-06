d2b1c7c75c15831be4d5fa307238a33aff7f6744
Sebastian Schalow <schalowseb@gmail.com>
2018-05-06 18:38:21 +0200

    Provide action generators instead of bare actions

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,11 +1,30 @@
 import {createStore} from 'redux';
 
-const store = createStore((state = {count: 0}, action) => {
+// Action generators
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy,
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+});
+
+const resetCount = () => ({
+  type: 'RESET',
+});
+
+const setCount = ({ count } = {}) => ({
+  type: 'SET',
+  count,
+});
+
+const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy,
+        count: state.count + action.incrementBy,
       };
     case 'DECREMENT':
       const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
@@ -29,33 +48,9 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-// -- ACTIONS -- //
-// Increment
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5,
-});
-
-// Decrement
-store.dispatch({
-  type: 'INCREMENT',
-});
-
-// Reset - set the count equal to zero
-store.dispatch({
-  type: 'RESET',
-});
-
-store.dispatch({
-  type: 'DECREMENT',
-});
-
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10,
-});
-
-store.dispatch({
-  type: 'SET',
-  count: 101,
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
+store.dispatch(incrementCount());
+store.dispatch(resetCount());
+store.dispatch(decrementCount());
+store.dispatch(decrementCount({ decrementBy: 10 }));
+store.dispatch(setCount({ count: 101 }));
