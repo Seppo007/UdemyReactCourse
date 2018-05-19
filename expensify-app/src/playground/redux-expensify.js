84d95b99c9e070d74a0748dc3501e44f5e5fbe84
Sebastian Schalow <schalowseb@gmail.com>
2018-05-19 23:03:44 +0200

    Add uuid dependency. Add functionality to add expenses via actions

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,6 +1,25 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
 // ADD_EXPENSE
+const addExpense = (
+  {
+    description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt,
+  }
+});
+
 // REMOVE_EXPENSE
 // EDIT_EXPENSE
 // SET_TEXT_FILTER
@@ -15,6 +34,11 @@ const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [
+        ...state,
+        action.expense,
+      ];
     default:
       return state;
   }
@@ -43,7 +67,12 @@ const store = createStore(combineReducers({
   filters: filtersReducer,
 }));
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
 
 const demoState = {
   expenses: [{
