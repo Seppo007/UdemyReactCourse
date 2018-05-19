40751bc0c5ff47b87f9b2466fa3ea780d8d197dc
Sebastian Schalow <schalowseb@gmail.com>
2018-05-19 22:14:52 +0200

    Setup expenses reducer and use combineReducer

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,5 +1,36 @@
 import { createStore, combineReducers } from 'redux';
 
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+// Expenses Reducer
+
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+// Filters Reducer
+// text => '', sortBy => 'date', startDate => undefined, endDate => undefined
+
+// Store creation
+
+const store = createStore(combineReducers({
+  expenses: expensesReducer,
+}));
+
+console.log(store.getState());
+
 const demoState = {
   expenses: [{
     id: 'fjwaifjwafogh',
