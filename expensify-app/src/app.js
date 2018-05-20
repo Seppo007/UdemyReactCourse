7fafd2263bd165bfef3e4385cad5ecd2c411b712
Sebastian Schalow <schalowseb@gmail.com>
2018-05-20 18:13:50 +0200

    Add expenses to store and filter by name

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -2,11 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from "./routers/AppRouter";
 import configureStore from './store/configureStore';
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
 
-console.log(store.getState());
+// addExpense -> Water bill
+// addExpense -> Gas bill
+// setTextFilter -> bill (2 items) -> water (1 item)
+// getVisibleExpenses -> print visible one to screen
+store.dispatch(addExpense({
+  description: 'Water bill',
+}));
+
+store.dispatch(addExpense({
+  description: 'Gas bill',
+}));
+
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+console.log(getVisibleExpenses(state.expenses, state.filters));
 
 ReactDOM.render(<AppRouter/>, document.getElementById('app'));
