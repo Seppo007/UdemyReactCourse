495f2cb2c74d0fa6d823206cae6cd684977a0678
Sebastian Schalow <schalowseb@gmail.com>
2018-05-25 12:49:02 +0200

    Add functionality to remove an expense

--- expensify-app/src/components/ExpenseListItem.js
+++ expensify-app/src/components/ExpenseListItem.js
@@ -1,14 +1,16 @@
-// Export stateless functional component
-// description, amount, createdAt
-
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from "../actions/expenses";
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ id, description, amount, createdAt, dispatch }) => (
   <div>
     <h3>{description}</h3>
     <p>{amount} - {createdAt}</p>
-    <button>Remove</button>
+    <button onClick={() => {
+      dispatch(removeExpense({ id }))
+    }}> Remove
+    </button>
   </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
