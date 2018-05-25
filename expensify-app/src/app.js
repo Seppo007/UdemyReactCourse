422f3634eded24b585c3c1d675afc59e827a45df
Sebastian Schalow <schalowseb@gmail.com>
2018-05-25 11:17:04 +0200

    Connect ExpenseList to store

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from "./routers/AppRouter";
+import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import { addExpense } from "./actions/expenses";
 import { setTextFilter } from "./actions/filters";
@@ -27,4 +28,10 @@ store.dispatch(setTextFilter('water'));
 const state = store.getState();
 console.log(getVisibleExpenses(state.expenses, state.filters));
 
-ReactDOM.render(<AppRouter/>, document.getElementById('app'));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
